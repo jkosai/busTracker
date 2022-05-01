b7253863c1d2e51b31b5312a14f25873c0458bc4
Jessica Kosai <jessica@kos.ai>
2022-05-01 12:14:43 -0700

    Add files via upload

---
+++ mapanimation.js
@@ -0,0 +1,69 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoiamtvc2FpIiwiYSI6ImNsMmtvMTBlMDA1azIzYnBqbmhvMjNhOTIifQ.dAHeuAStdgJoNGAZBoCmZQ';
+  
+var map;
+var markers = [];
+
+// load map
+function init(){
+
+	var element = document.getElementById('map');
+  	map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: [-71.104081, 42.365554],
+        zoom: 14,
+      });
+  	addMarkers();
+}
+
+// Add bus markers to map
+async function addMarkers(){
+	var locations = await getBusLocations();
+	locations.forEach(function(bus){
+		var marker = getMarker(bus.id);	
+		if (marker){
+			moveMarker(marker,bus);
+		}
+		else{
+			addMarker(bus);			
+		}
+	});
+	console.log(new Date());
+	setTimeout(addMarkers,1000);
+}
+
+// Request bus data from MBTA
+async function getBusLocations(){
+	var url = 'https://api-v3.mbta.com/vehicles?api_key=ca34f7b7ac8a445287cab52fb451030a&filter[route]=1&include=trip';	
+	var response = await fetch(url);
+	var json     = await response.json();
+	return json.data;
+}
+
+function addMarker(bus){
+	var marker = new mapboxgl.Marker().setLngLat({lat: bus.attributes.latitude, 
+        lng: bus.attributes.longitude}).addTo(map);
+        marker._element.id = bus.id;
+    
+	markers.push(marker);
+    
+}
+
+
+function moveMarker(marker,bus) {
+    marker.setLngLat({
+        lat: bus.attributes.latitude, 
+        lng: bus.attributes.longitude
+    })
+	
+}
+
+function getMarker(id){
+    console.log(markers);
+	var marker = markers.find(function(item){
+		return item._element.id === id;        
+	});
+	return marker;
+}
+
+window.onload = init;
